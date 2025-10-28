2c48540fa6e72e14a06f45da8b1bd39a420ca443
mrkalifa1999-cyber <mrkalifa1999@gmail.com>
2025-10-28 22:22:47 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " blinder~1QRnlKgC#Q9RDQUbOP5YF0vQF0zxKfonhxRMNJdKOy9xrTIfKWbU",  // add your Session Id make sure it starts with blinder~
+  SESSION_ID: " blinder~EJxmwSgK#x3eHSoiBEWsEdJA9cqDAqL0y86blkYbzGo1vYRHG2gg",  // add your Session Id make sure it starts with blinder~
   OWNER_NUMBER: "+94761680869", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
